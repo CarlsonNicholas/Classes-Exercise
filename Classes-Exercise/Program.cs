d8319327679f4bc0578be54959e0ea7dc433b2a4
Nick Carlson <CarlsonNicholas82@gmail.com>
2022-12-07 15:35:22 -0500

    Added better text

--- Classes-Exercise/Program.cs
+++ Classes-Exercise/Program.cs
@@ -7,4 +7,4 @@ car_1.Make = "Honda";
 car_1.Model = "CRV";
 car_1.Year = 2011;
 
-Console.WriteLine($"Car 1 is a {car_1.Make} the model is a {car_1.Model} it was maufactured in the year {car_1.Year}");
+Console.WriteLine($"Car 1 is a {car_1.Make} the model is a {car_1.Model}; it was maufactured in the year {car_1.Year}!");
