2a788e8a3afa3f03904a22b245e55b1dc5254e85
Nick Carlson <CarlsonNicholas82@gmail.com>
2022-12-07 15:23:44 -0500

    inital

---
+++ Classes-Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿using Classes_Exercise;
+
+Car car_1 = new Car();
+
+
+car_1.Make = "Honda";
+car_1.Model = "CRV";
+car_1.Year = 2011;
+
+Console.WriteLine($"Car 1 is a {car_1.Make} the model is a {car_1.Model} it was maufactured in the year {car_1.Year}");
